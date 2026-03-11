acea8f083fd12f453b3ce9f6926dd34979358654
sat7312 <sat7312@gmail.com>
2026-03-11 19:29:34 +0900

    10,11 글 단건, 댓글 단건, 다건 조회 구현

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -6,17 +6,20 @@ import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
 public class ApiV1PostController {
 
     private final PostService postService;
 
-    @GetMapping("/api/v1/posts")
+    @GetMapping
     @ResponseBody
     public List<PostDto> list() {
         List<Post> result = postService.findAll();
@@ -26,5 +29,12 @@ public class ApiV1PostController {
                 .toList();
 
         return postDtoList;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public PostDto deiail(@PathVariable int id) {
+        Post post = postService.findById(id).get();
+        return new PostDto(post);
     }
 }
