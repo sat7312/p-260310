fbc0ba0dca2484bdc3b3c77ce75ffd8ac66db608
sat7312 <sat7312@gmail.com>
2026-03-12 15:14:08 +0900

    38 정확한 날짜 비교를 위해 DB에서 Post 조회후 패턴으로 확인

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.matchesPattern;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -60,10 +61,14 @@ public class ApiV1PostControllerTest {
                 .andExpect(handler().methodName("detail"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.createDate").exists())
-                .andExpect(jsonPath("$.modifyDate").exists())
                 .andExpect(jsonPath("$.title").value("제목1"))
                 .andExpect(jsonPath("$.content").value("내용1"));
+
+
+        Post post = postRepository.findById(targetId).get();
+        resultActions
+                .andExpect(jsonPath("$.createDate").value(matchesPattern(post.getCreateDate().toString().replaceAll("0+$", "") + ".*")))
+                .andExpect(jsonPath("$.modifyDate").value(matchesPattern(post.getModifyDate().toString().replaceAll("0+$", "") + ".*")))
     }
 
     @Test
