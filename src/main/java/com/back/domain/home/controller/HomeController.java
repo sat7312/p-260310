26982d2374f4e4d6fe866a47cca992e3305c7811
sat7312 <sat7312@gmail.com>
2026-03-13 14:24:17 +0900

    53,54 SpringDocConfig 추가, 문서의 제목/버전/설명 추가, 프론트엔드 개발자를 위한 메인페이지 구현, HomeController 추가

---
+++ src/main/java/com/back/domain/home/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.back.domain.home.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home() throws UnknownHostException {
+
+        InetAddress localhost = InetAddress.getLocalHost();
+
+        return """
+                <h1>Welcome to Rest1</h1>
+                <p>Server IP Address: %s</p>
+                <p>Server Host Name: %s</p>
+                <div>
+                    <a href="swagger-ui/index.html">API 문서로 이동</a>
+                </div>
+                """.formatted(localhost.getHostAddress(), localhost.getHostName());
+    }
+
+}
